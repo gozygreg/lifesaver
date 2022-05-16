3d974dfe44bce78a2ac590fcb651f413d6f05df8
gozygreg <goziennabuife@yahoo.com>
2022-05-16 23:36:43 +0000

    add script to forward, save and display username to quizgame page then add script to obtain players username from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
+// Obtain player's username for local storage. Username is required to play quiz.
+
+let myName = localStorage.getItem("name");
+    document.getElementById("hey").innerHTML = myName;
 
