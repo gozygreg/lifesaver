6bc48cd2044feccf3e297d776e0067ed8e1d98f6
gozygreg <goziennabuife@yahoo.com>
2022-05-20 01:59:25 +0000

    Create variable  called score to store answer, also create currentquestion variable needed to assess the right question and finally create a vairable that contain  an array of our questions with options to choose from

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
             } if (this.getAttribute("data-type") === "submit") {
                 alert("submiting!");
             } else {
-                let nextStep = this.getAttribute("data-type");
-                alert(`You clicked ${nextStep}`);
+                let Step = this.getAttribute("data-type");
+                alert(`You clicked ${Step}`);
             }
         })
     }
@@ -28,9 +28,71 @@ document.addEventListener("DOMContentLoaded", function () {
 // create variable to access the elements via the DOM
 const submitBtn = document.getElementById("submit");
 const nextStepBtn = document.getElementById("next-step");
+const prevStepBtn = document.getElementById("prev-step")
 const question = document.getElementById("question");
 const correctBtn = document.getElementById("correct");
 const wrongBtn = document.getElementById("wrong");
+
+// Create a viarable for current question. To access the right question by increamenting or decrementing this counter based on user's interactions with next and prev buttons
+let currentQuestion = 0;
+
+// Create a counter viarable for the score
+let score = 0;
+
+// Create an array of questions with options to choose from
+let questions = [
+    {
+        question: "What do you do before you approach a person who has collapse?",
+        answers: [
+            {option: "Shout for help!", answer: true},
+            {option: "Shake them gently", answer: false}
+        ]
+    },
+    {
+        question: "What do you do after you have shouted for help?",
+        answers: [
+            {option: "Shake them gently", answer: true},
+            {option: "Look and listen for signs of normal breathing", answer: false}
+        ]
+    },
+    {
+        question: "What do you do after you shake them gently and they done respond?",
+        answers: [
+            {option: "Look and listen for signs of normal breathing", answer: true},
+            {option: "", answer: false}
+        ]
+    },
+    {
+        question: "What do you do after you have look and listened for signs of normal breathing and there is none?",
+        answers: [
+            {option: "Fetch a defibrilator if you can", answer: true},
+            {option: "Begin to cry and panic", answer: false}
+        ]
+    },
+    {
+        question: "What do you do after you have fetch the defibrillator?",
+        answers: [
+            {option: "Start chest compression immediately", answer: true},
+            {option: "Expose their chest and think of chest compression", answer: false}
+        ]
+    },
+    {
+        question: "What do you do after you have done some chest compression?",
+        answers: [
+            {option: "If you have a defibrillator, switch it on and follow the instructions", answer: true},
+            {option: "Throw the defibrillator away", answer: false}
+        ]
+    },
+    {
+        question: "What do you do after you have followed the instruction given by the defibrillator?",
+        answers: [
+            {option: "Resume or continue CPR until a paramedic arrives and tell you what to do or the person show signs of life", answer: true},
+            {option: "Stop CPR as you are tired", answer: false}
+        ]
+    }
+]
+
+
 
 
 
