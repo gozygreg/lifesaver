53294e6836824874afdef2c4b050b0c938d0b30c
gozygreg <goziennabuife@yahoo.com>
2022-05-29 19:02:59 +0000

    Create a new script.js file to forward, save and display username to quizgame page

---
+++ assets/js/script2.js
@@ -0,0 +1,19 @@
+/* script to forward, save and display username to quizgame page */
+    
+let startButton = document.getElementById("begin");
+let userName = document.getElementById("username");
+    userName.focus();
+
+
+    userName.addEventListener("keydown", function() {
+        if (event.key === 'Enter') {
+            let theUserName = document.getElementById('username').value;
+            localStorage.setItem("name", theUserName);
+        }
+    })
+
+    startButton.addEventListener("click", function() {
+        let theUserName = document.getElementById("username").value;
+        localStorage.setItem("name", theUserName);
+    });
+    
